276a6033499919801635d1f4fdc90282d1a87a90
Yari PL <yaroslav.khudchenko@gmail.com>
2019-10-10 00:51:30 +0200

    calculate on componentDidMount + comments

--- src/Grid.js
+++ src/Grid.js
@@ -2,32 +2,32 @@ import React, {Component} from 'react';
 
 
 export default class Grid extends Component { 
-      /*   constructor(props) {
+        constructor(props) {
             super(props);
-        } */
+            this.state = {
+                nodes : []
+            }
+        } 
+
+        componentDidMount() {
+            let nodes = [];
+            //let columns = [];
+            for (let i = 0; i < 20; i++) {
+                nodes.push([]); // push array to display row
+                for (let j = 0; j < 20; j++) {
+                    nodes[i].push(<div key={j} className="oneNode"></div>); // every node(column)
+                }
+            }
+            this.setState({nodes})
+        }
 
         render() {
-            let rows = [];
-            //let columns = [];
-            for(let i = 0; i < 20;i++) {
-                rows.push([]);
-                for (let j = 0; j < 20; j++) {
-                  rows[i].push(<div key={j} className="oneNode"></div>);
-                }
-
-            }
-            
-            console.log(rows);
+           let {nodes} = this.state;
             return (
               <div className="grid">
-                
-               
-                {rows.map((s) => {
-                        return <div>
-                            {s.map((c) => c)} 
-                        </div>
-                    })};
-                }
+                {nodes.map(r => { // get every row(main array)
+                  return <div>{r.map(c => c)}</div>; // build node based on main array index(20 per row)
+                })}
               </div>
             );
         }
