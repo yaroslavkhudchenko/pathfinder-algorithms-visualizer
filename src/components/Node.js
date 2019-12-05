c7a7f0daa193cd079f2561d6e53907f639b914f4
Yari PL <yaroslav.khudchenko@gmail.com>
2019-12-05 22:30:21 +0100

    set color on click

--- src/components/Node.js
+++ src/components/Node.js
@@ -18,7 +18,10 @@ export default class Node extends Component {
                             row={this.props.row} 
                             column={this.props.column} 
                             // onClick={this.props.clickNode}
-                            className={`singleNode${this.props.isStart ? ' start': this.props.isTarget ? ' target': ''}`} // set good color for start and target nodes
+                            className={`singleNode${
+                                this.props.row === context.startNode.row && this.props.column === context.startNode.column ? ' start': 
+                                this.props.row === context.targetNode.row && this.props.column === context.targetNode.column ? ' target': ''}`
+                            } // set good color for start and target nodes
                             id={`node-${this.props.row}-${this.props.column}`} // id for future animation
                             onClick={context.setStartTargetNodes}
                         > 
